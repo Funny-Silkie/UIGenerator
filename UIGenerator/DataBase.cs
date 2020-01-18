8c75398ea93928d66f9b4b60cdb8bd04865991b3
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-01-19 00:55:56 +0900

    エラー対策

--- UIGenerator/DataBase.cs
+++ UIGenerator/DataBase.cs
@@ -66,12 +66,12 @@ namespace UIGenerator
         /// <summary>
         /// 既定のフォントを取得する
         /// </summary>
-        public static DynamicFontInfo DefaultFont => _defaultFont ?? (_defaultFont = DynamicFontInfo.GetInstance("Resource/NotoSerifCJKjp-Medium.otf", 30, new ColorPlus(ColorSet.White), 1, new ColorPlus(ColorSet.Black)));
+        public static DynamicFontInfo DefaultFont => _defaultFont ?? (_defaultFont = DynamicFontInfo.GetInstance("NotoSerifCJKjp-Medium.otf", 30, new ColorPlus(ColorSet.White), 1, new ColorPlus(ColorSet.Black)));
         private static DynamicFontInfo _defaultFont;
         /// <summary>
         /// 既定の画像を取得する
         /// </summary>
-        public static TextureInfo DefaultTexture => _defaultTexture ?? (_defaultTexture = TextureInfo.GetInstance("Resource/DefaultPicture.png"));
+        public static TextureInfo DefaultTexture => _defaultTexture ?? (_defaultTexture = TextureInfo.GetInstance("DefaultPicture.png"));
         private static TextureInfo _defaultTexture;
         /// <summary>
         /// 管理されているフォントを取得する
--- UIGenerator/Program.cs
+++ UIGenerator/Program.cs
@@ -14,17 +14,18 @@ namespace UIGenerator
         static void Main(string[] args)
         {
             Engine.Initialize("NewProject", 640, 480, new EngineOption());
-            Engine.File.AddRootPackageWithPassword("DefaultResource.pack", "");
+            Engine.File.AddRootPackage("DefaultResource.pack");
             DataBase.Initialize(640, 480, "NewProject");
-            var form = new MainEdittor();
-            form.Show();
-            Engine.ChangeScene(new MainScene());
-            while (ContinueUpdating & Engine.DoEvents())
+            using (var form = new MainEdittor())
             {
-                Application.DoEvents();
-                Engine.Update();
+                form.Show();
+                Engine.ChangeScene(new MainScene());
+                while (ContinueUpdating & Engine.DoEvents())
+                {
+                    Application.DoEvents();
+                    Engine.Update();
+                }
             }
-            if (!form.IsDisposed) form.Dispose();
             Engine.Terminate();
         }
     }
